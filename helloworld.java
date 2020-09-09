251eafb5f13586bba1f28ce2a167ebe10e2185ed
Irsyaad <30659333+irsyaad12@users.noreply.github.com>
2020-09-09 11:54:10 +0700

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld{
+     		public static void main(String[] l) {
+			String nama, kelas, nim;
+		nama = "Irsyaadul_Ibaad";
+		kelas = "D3IF4401";
+		nim = "6706202089";
+
+	System.out.println("Nama = " + nama);
+	System.out.println("NIM = " + nim);
+	System.out.println("Kelas = " + kelas);
+	}
+}
